0664a0a94f243669e455295c9ef942f76ab75a94
i_am_yevhen <yevheniiperekopskyi@gmail.com>
2020-04-30 17:24:34 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Best React App</h1>
+      <p className="lead">Version 1.0.42</p>
+    </div>
+  </div>
+)
+
+export default About;
